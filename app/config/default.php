48323f9851b842309ce5356487f1510208a4890a
Diego Tejada <diegotejadav@gmail.com>
2021-01-30 17:43:53 -0500

    Updated config max_drones & max_coverage

--- app/config/default.php
+++ app/config/default.php
@@ -4,8 +4,8 @@
  */
 
 return [
-    'max_drones' => 10, // Can deploy 10 drones at time
+    'max_drones' => 20, // Can deploy 20 drones at time
     'dron_deliveries_per_time' => 3, // Each drone can carry maximum 3 deliveries per time
-    'max_coverage' => 10,
+    'max_coverage' => 10, // The amount of blocks around that drones have of coverage
 ];
 ?>
