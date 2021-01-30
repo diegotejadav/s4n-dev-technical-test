94ca4084383b006b9a2f83fc4ca25994ee29d017
Diego Tejada <diegotejadav@gmail.com>
2021-01-30 18:00:33 -0500

    Passed new param to Dron instance

--- app/index.php
+++ app/index.php
@@ -14,12 +14,16 @@ function start()
 
     $drones = Util::getInputFiles();
 
-    foreach ($drones as $inputName => $paths) {
-        $dron = new Dron($paths, $config['dron_deliveries_per_time']);
-        // var_dump($paths); // TODO: Clear this comment
-        $outputFileName = str_replace('in', 'out', $inputName);
-        $dron->startDelivery($outputFileName);
+    // TODO: The drones deliveries should be called with some parallel processing, so 
+    // they can run simultaneously
+    foreach ($drones as $fileName => $paths) {
+        $outputFileName = Util::outputFilename($fileName);
+        $dron = new Dron($paths, $config['dron_deliveries_per_time'], $config['max_coverage'], $outputFileName);
+        $dron->startDelivery();
+        unset($dron);
     }
+
+    echo("Finished deliveries");
 }
 
 start();
